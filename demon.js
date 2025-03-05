3cb6e99dd55c804a3600c32492aa82f2c54a987e
DEV'S NATION HUB <beratech00@gmail.com>
2025-03-05 21:18:50 +0300

    Update demon.js

--- demon.js
+++ demon.js
@@ -57,7 +57,7 @@ async function downloadSessionData() {
         console.error('Please add your session to SESSION_ID env !!');
         return false;
     }
-    const sessdata = config.SESSION_ID.split("Joel-Md&")[1];
+    const sessdata = config.SESSION_ID.split("JOEL~XMD~")[1];
     const url = `https://pastebin.com/raw/${sessdata}`;
     try {
         const response = await axios.get(url);
