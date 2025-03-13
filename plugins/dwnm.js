0c3beb546cb7aafe18a5f1d5a9369a10117d8425
DEV'S NATION HUB <beratech00@gmail.com>
2025-03-13 17:46:42 +0300

    A

--- plugins/dwnm.js
+++ plugins/dwnm.js
@@ -100,7 +100,7 @@ const test = async (m, Matrix) => {
 ╰──────────────┈⊷`;
 
     await Matrix.sendMessage(m.from, {
-      image: fs.readFileSync('./media/slayer.jpg'),
+      image: fs.readFileSync('./media/slayer3.jpg'),
       caption: str,
       contextInfo: {
         mentionedJid: [m.sender],
